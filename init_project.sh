fd1708e938bcaecfea02277d209741650a7c05a2
MasaoSasaki <daikichi852@icloud.com>
2021-03-14 14:54:04 +0900

    :loud_sound: Add check install GitHub CLI

--- init_project.sh
+++ init_project.sh
@@ -14,22 +14,24 @@ function ConfirmExecution() {
 }
 function ExistError() {
   echo "${ESC}[0;31mError: $1${ESC}[0;39m"
-  if [ $2 = 'rm' ]; then cd .. && rm -rf $3; fi
+  if [ "$2" = "rm" ]; then cd .. && rm -rf $3; fi
   exit
 }
 
-# # Gitのインストールチェック
+# Gitのインストールチェック
 git --version
 if [ $? != 0 ]; then ExistError 'gitが見つかりません。インストールされていることを確認してください。'; fi
+# GitHub CLIのインストールチェック
+gh --version
+if [ $? != 0 ]; then ExistError 'GitHub CLIが見つかりません。インストールされていることを確認してください。'; fi
 
 # プロジェクト名のチェック
 if [ -z $1 ]; then ExistError 'プロジェクト名が指定されていません。'; fi
 if [ $1 = '.' -o $1 = '..' ]; then ExistError "プロジェクト名'$1'は指定できません。"; fi
 if [ $1 = '.' -o $1 = '..' ]; then ExistError "プロジェクト名'$1'は指定できません。"; fi
-if [ !`echo $1 | wc -m` -le 101 ]; then ConfirmExecution '100文字までが名前として有効になります。'; fi
 if [ `echo $1 | egrep "\!|\#|\%|\&|\(|\)|\=|\^|\~|\¥|\@|\[|\{|\;|\+|\:|\*|\]|\}|\,|\.|/|\?"` ]; then ConfirmExecution '名前の一部が"-(ハイフン)"に置き換わります。'; fi
 
-# # GitHubユーザー名の取得
+# GitHubユーザー名の取得
 username=`git config user.name`
 if [ $? != 0 ]; then ExistError 'GitHubユーザー名が見つかりませんでした。'; fi
 
@@ -47,8 +49,8 @@ if [ $? != 0 ]; then ExistError 'git commitに失敗しました。' 'rm' $1; fi
 git branch -M main
 if [ $? != 0 ]; then ExistError 'ブランチの作成に失敗しました。' 'rm' $1; fi
 
-# # privateリポジトリの場合はオプション'--public'を'--private'に変更'
-# # originから変更する場合はオプション'--confirm'を削除'
+# privateリポジトリの場合はオプション'--public'を'--private'に変更'
+# originから変更する場合はオプション'--confirm'を削除'
 gh repo create --public --confirm $1
 if [ $? != 0 ]; then ExistError '新規リポジトリが作成できませんでした。'; fi
 

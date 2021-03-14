657a28810ba18cd847f339419227c2a391e731c4
MasaoSasaki <daikichi852@icloud.com>
2021-03-14 13:08:39 +0900

    :recycle: Update refactoring

--- init_project.sh
+++ init_project.sh
@@ -1,107 +1,58 @@
 #!/bin/sh
+ESC=$(printf '\033')
 
-# Gitのインストールチェック
-git --version
-if [ $? != 0 ]; then
-  if [ $SHELL = '/bin/zsh' ]; then
-    echo "\033[0;31mgitが見つかりません。インストールされていることを確認してください。\033[0;39m"
+function ConfirmExecution() {
+  echo "${ESC}[0;33mWarning: $1継続しますか？ [y/n] ${ESC}[0;39m"
+  read input
+
+  if [ $input = 'y' ]; then
+    echo "${ESC}[0;33mスクリプトを継続します。${ESC}[0;39m"
   else
-    echo -e "\e[31mgitが見つかりません。インストールされていることを確認してください。\e[m"
+    echo "${ESC}[0;33mスクリプトを中止します。${ESC}[0;39m"
+    exit
   fi
+}
+function ExistError() {
+  echo "${ESC}[0;31mError: $1${ESC}[0;39m"
+  if [ $2 = 'rm' ]; then cd .. && rm -rf $3; fi
   exit
-fi
+}
+
+# # Gitのインストールチェック
+git --version
+if [ $? != 0 ]; then ExistError 'gitが見つかりません。インストールされていることを確認してください。'; fi
 
 # プロジェクト名のチェック
-if [ -z $1 ]; then
-  if [ $SHELL = '/bin/zsh' ]; then
-    echo "\033[0;31mプロジェクト名が指定されていません。\033[0;39m"
-  else
-    echo -e "\e[31mプロジェクト名が指定されていません。\e[m"
-  fi
-  exit
-fi
-if [ $1 = '.' -o $1 = '..' ]; then
-  if [ $SHELL = '/bin/zsh' ]; then
-    echo "\033[0;31mプロジェクト名'$1'は設定できません。\033[0;39m"
-  else
-    echo -e "\e[31mプロジェクト名'$1'は設定できません。\e[m"
-  fi
-  exit
-fi
+if [ -z $1 ]; then ExistError 'プロジェクト名が指定されていません。'; fi
+if [ $1 = '.' -o $1 = '..' ]; then ExistError "プロジェクト名'$1'は指定できません。"; fi
+if [ $1 = '.' -o $1 = '..' ]; then ExistError "プロジェクト名'$1'は指定できません。"; fi
+if [ !`echo $1 | wc -m` -le 101 ]; then ConfirmExecution '100文字までが名前として有効になります。'; fi
+if [ `echo $1 | egrep "\!|\#|\%|\&|\(|\)|\=|\^|\~|\¥|\@|\[|\{|\;|\+|\:|\*|\]|\}|\,|\.|/|\?"` ]; then ConfirmExecution '名前の一部が"-(ハイフン)"に置き換わります。'; fi
 
-# GitHubユーザー名の取得
+# # GitHubユーザー名の取得
 username=`git config user.name`
-if [ -z $1 ]; then
-  if [ $SHELL = '/bin/zsh' ]; then
-    echo "\033[0;31mユーザー名が見つかりませんでした。\033[0;39m"
-  else
-    echo -e "\e[31mユーザー名が見つかりませんでした。\e[m"
-  fi
-  exit
-fi
+if [ $? != 0 ]; then ExistError 'GitHubユーザー名が見つかりませんでした。'; fi
 
 mkdir $1
-if [ -z $1 ]; then
-  if [ $SHELL = '/bin/zsh' ]; then
-    echo "\033[0;31m$1は既に存在します。\033[0;39m"
-  else
-    echo -e "\e[31m$1は既に存在します。\e[m"
-  fi
-  exit
-fi
+if [ $? != 0 ]; then ExistError "$1は既に存在します。"; fi
 echo "# $1" >> $1/README.md ; cd $1
 
 git init
-if [ $? != 0 ]; then
-  if [ $SHELL = '/bin/zsh' ]; then
-    echo "\033[0;31mgit initに失敗しました。\033[0;39m"
-  else
-    echo -e "\e[31mgit initに失敗しました。\e[m"
-  fi
-  cd .. && rm -rf $1
-  exit
-fi
+if [ $? != 0 ]; then ExistError 'git initに失敗しました。' 'rm' $1; fi
 
 git add README.md
 git commit -m ":tada:first commit"
-if [ $? != 0 ]; then
-  if [ $SHELL = '/bin/zsh' ]; then
-    echo "\033[0;31mgit commitに失敗しました。\033[0;39m"
-  else
-    echo -e "\e[31mgit commitに失敗しました。\e[m"
-  fi
-  cd .. && rm -rf $1
-  exit
-fi
+if [ $? != 0 ]; then ExistError 'git commitに失敗しました。' 'rm' $1; fi
 
 git branch -M main
-if [ $? != 0 ]; then
-  if [ $SHELL = '/bin/zsh' ]; then
-    echo "\033[0;31mブランチの作成に失敗しました。\033[0;39m"
-  else
-    echo -e "\e[31mブランチの作成に失敗しました。\e[m"
-  fi
-  cd .. && rm -rf $1
-  exit
-fi
+if [ $? != 0 ]; then ExistError 'ブランチの作成に失敗しました。' 'rm' $1; fi
 
-# privateリポジトリの場合はオプション'--public'を'--private'に変更'
-# originから変更する場合はオプション'--confirm'を削除'
+# # privateリポジトリの場合はオプション'--public'を'--private'に変更'
+# # originから変更する場合はオプション'--confirm'を削除'
 gh repo create --public --confirm $1
-if [ $? != 0 ]; then
-  if [ $SHELL = '/bin/zsh' ]; then
-    echo "\033[0;31m新規リポジトリが作成できませんでした。\033[0;39m"
-  else
-    echo -e "\e[31m新規リポジトリが作成できませんでした。\e[m"
-  fi
-  exit
-fi
+if [ $? != 0 ]; then ExistError '新規リポジトリが作成できませんでした。'; fi
 
 git remote add origin https://github.com/$username/$1.git
 git push -u origin main
 
-if [ $SHELL = '/bin/zsh' ]; then
-  echo "\033[0;32m新規プロジェクト'$1'の作成が完了しました。\033[0;39m"
-else
-  echo -e "\e[31m新規プロジェクト'$1'の作成が完了しました。\e[m"
-fi
+echo "${ESC}[0;32mSuccess: 新規プロジェクト'$1'の作成が完了しました。${ESC}[0;39m"
